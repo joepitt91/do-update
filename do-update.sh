19c8279f7af2bf92d10d9dc31b4a39aebd248277
Joe Pitt <Joe.Pitt@joepitt.co.uk>
2020-01-01 14:48:37 +0000

    Minor tidying

--- do-update.sh
+++ do-update.sh
@@ -22,92 +22,87 @@ echo 'This is free software, and you are welcome to redistribute it under certai
 if [ "$(id -u)" != "0" ]; then
 	if hash sudo 2>/dev/null; then
 		# Self-Elevate to root
-		sudo $0
+		sudo $0 $@
 		exit $?
 	else
-		echo "do-update must be run as root, however, sudo is not installed so I cannot self elevate."
+		echo "do-update must be run as root, however, sudo is not installed so cannot self elevate."
 		exit 1
 	fi
 fi
 
 if ! hash apt-get 2>/dev/null; then
-  echo "do-update only works with apt-get, however, this system does not use apt-get."
-  exit 2
+	echo "do-update only works with apt-get, however, this system does not use apt-get."
+	exit 2
 fi
 
 if ! hash logger 2>/dev/null; then
-  echo 'do-update requires logger to log its activity to syslog, please install it using:'
-  echo '  sudo apt-get install util-linux'
-  exit 3
+	echo 'do-update requires logger to log its activity to syslog, please install it using:'
+	echo '  sudo apt-get install util-linux'
+	exit 3
 fi
 
-# Prevent errors and hanging
+# Prevent apt from prompting for user input
 export TERM=${TERM:-dumb}
 export DEBIAN_FRONTEND=noninteractive
 
-clear
-
-echo 'do-update 1.5. 2017-11-01. Copyright (c) Joe Pitt, www.joepitt.co.uk'
 echo
-
 echo 'Updating Package List...'
 logger '[Info] do-update: Starting Package List Update'
-apt-get update -y > /dev/null 2>&1
+apt-get update -y >/dev/null 2>&1
 if [ "$?" == "0" ]; then
-  logger '[Info] do-update: Finished Package List Update'
+	logger '[Info] do-update: Finished Package List Update'
 else
-  logger -s '[Error] do-update: Package List Update Failed'
-  exit 10
+	logger -s '[Error] do-update: Package List Update Failed'
+	exit 10
 fi
 
 echo 'Upgrading Packages...'
 logger '[Info] do-update: Starting Package Upgrades'
-apt-get upgrade -y > /dev/null 2>&1
+apt-get upgrade -y >/dev/null 2>&1
 if [ "$?" == "0" ]; then
-  logger '[Info] do-update: Finished Package Upgrades'
+	logger '[Info] do-update: Finished Package Upgrades'
 else
-  logger -s '[Error] do-update: Package Upgrades Failed'
-  exit 11
+	logger -s '[Error] do-update: Package Upgrades Failed'
+	exit 11
 fi
 
 echo 'Upgrading Distribution...'
 logger '[Info] do-update: Starting Distribution Upgrade'
-apt-get dist-upgrade -y > /dev/null 2>&1
+apt-get dist-upgrade -y >/dev/null 2>&1
 if [ "$?" == "0" ]; then
-  logger '[Info] do-update: Finished Distribution Upgrade'
+	logger '[Info] do-update: Finished Distribution Upgrade'
 else
-  logger -s '[Error] do-update: Distribution Upgrade Failed'
-  exit 12
+	logger -s '[Error] do-update: Distribution Upgrade Failed'
+	exit 12
 fi
 
 echo 'Removing Unneeded Packages...'
 logger '[Info] do-update: Starting Unneeded Package Removal'
-apt-get autoremove -y > /dev/null 2>&1
+apt-get autoremove -y >/dev/null 2>&1
 if [ "$?" == "0" ]; then
-  logger '[Info] do-update: Finished Unneeded Package Removal'
+	logger '[Info] do-update: Finished Unneeded Package Removal'
 else
-  logger -s '[Error] do-update: Unneeded Package Removal Failed'
-  exit 13
+	logger -s '[Error] do-update: Unneeded Package Removal Failed'
+	exit 13
 fi
 
 echo 'Deleting Old Packages...'
 logger '[Info] do-update: Starting Old Package Deletion'
-apt-get autoclean -y > /dev/null 2>&1
+apt-get autoclean -y >/dev/null 2>&1
 if [ "$?" == "0" ]; then
-  logger '[Info] do-update: Finished Old Package Deletion'
+	logger '[Info] do-update: Finished Old Package Deletion'
 else
-  logger -s '[Error] do-update: Old Package Deletion Failed'
-  exit 14
+	logger -s '[Error] do-update: Old Package Deletion Failed'
+	exit 14
 fi
-
 
 # Determine if reboot is required
 if [ -a /var/run/reboot-required ]; then
 	if [ "$1" == "-y" ]; then
 		echo "Rebooting System..."
-		sleep 1
 		logger '[Info] do-update: Reboot needed, -y flag set, rebooting now'
-		reboot > /dev/null 2>&1
+		sleep 1
+		reboot >/dev/null 2>&1
 	elif [ "$1" == "-n" ]; then
 		echo "Done. Reboot Required!"
 		logger '[Notice] do-update: Reboot needed, -n flag set, not rebooting'
@@ -116,9 +111,9 @@ if [ -a /var/run/reboot-required ]; then
 		read answer
 		if [ $answer == "yes" ]; then
 			echo "Rebooting System..."
-			sleep 1
 			logger '[Info] do-update: Reboot needed, user requested reboot, rebooting now'
-			reboot > /dev/null 2>&1
+			sleep 1
+			reboot >/dev/null 2>&1
 		else
 			logger '[Notice] do-update: Reboot needed, user declined reboot, not rebooting'
 		fi
