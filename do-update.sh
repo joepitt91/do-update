6bd48157859dbec5bc0dd76a1f31c0aafad6af02
Joe Pitt <Joe.Pitt@joepitt.co.uk>
2020-01-01 14:47:37 +0000

    Move to GPLv3

--- do-update.sh
+++ do-update.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
-#
-# Name:    do-update
-# Version: 1.5 (2017-11-01)
-# URL:     https://www.joepitt.co.uk/Project/do-update/
-#
-# This work is licensed under the Creative Commons
-# Attribution-NonCommercial-ShareAlike 4.0 International License. To view a
-# copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
+# do-update v1.6 (2020-01-01)
+# https://www.joepitt.co.uk/Project/do-update/
+# Copyright (C) 2020  Joe Pitt
+
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+# version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with this program. If not, see
+# <https://www.gnu.org/licenses/>.
+
+echo 'do-update v1.6 (2020-01-01)  Copyright (C) 2020  Joe Pitt'
+echo 'This program comes with ABSOLUTELY NO WARRANTY.'
+echo 'This is free software, and you are welcome to redistribute it under certain conditions; see ' \
+' https://www.gnu.org/licenses/gpl-3.0.txt for details.'
 
 # Check running as root
 if [ "$(id -u)" != "0" ]; then
